8468902d5c74cb68725774a2d242d8563710e48a
tomaszrykala <tomasz.rykala@c.zen.ly>
2022-08-24 10:33:32 +0100

    Use AndroidX CustomTabsLauncher only

--- app/src/main/java/com/tomaszrykala/githubbrowser/compose/ui/theme/Theme.kt
+++ app/src/main/java/com/tomaszrykala/githubbrowser/compose/ui/theme/Theme.kt
@@ -6,15 +6,17 @@ import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 
+val primaryVariant = Purple700
+
 private val DarkColorPalette = darkColors(
     primary = Purple200,
-    primaryVariant = Purple700,
+    primaryVariant = primaryVariant,
     secondary = Teal200
 )
 
 private val LightColorPalette = lightColors(
     primary = Purple500,
-    primaryVariant = Purple700,
+    primaryVariant = primaryVariant,
     secondary = Teal200
 
     /* Other default colors to override
--- app/src/main/java/com/tomaszrykala/githubbrowser/compose/ui/util/CustomTabsLauncher.kt
+++ app/src/main/java/com/tomaszrykala/githubbrowser/compose/ui/util/CustomTabsLauncher.kt
@@ -3,27 +3,38 @@ package com.tomaszrykala.githubbrowser.compose.ui.util
 import android.content.Context
 import android.net.Uri
 import androidx.annotation.MainThread
+import androidx.browser.customtabs.CustomTabColorSchemeParams
 import androidx.browser.customtabs.CustomTabsIntent
 import androidx.browser.customtabs.CustomTabsIntent.COLOR_SCHEME_SYSTEM
 import androidx.browser.customtabs.CustomTabsIntent.SHARE_STATE_ON
+import androidx.compose.ui.graphics.toArgb
 import com.tomaszrykala.githubbrowser.compose.R
-import saschpe.android.customtabs.CustomTabsHelper
-import saschpe.android.customtabs.WebViewFallback
+import com.tomaszrykala.githubbrowser.compose.ui.theme.primaryVariant
 import javax.inject.Inject
 
 class CustomTabsLauncher @Inject constructor() {
 
     @MainThread
     fun launch(uri: Uri, context: Context) {
-        val intent = CustomTabsIntent.Builder()
+        CustomTabsIntent.Builder()
             .setShareState(SHARE_STATE_ON)
             .setColorScheme(COLOR_SCHEME_SYSTEM)
             .setStartAnimations(context, R.anim.slide_in_right, R.anim.slide_out_left)
             .setExitAnimations(context, R.anim.slide_in_left, R.anim.slide_out_right)
+            .setDefaultColorSchemeParams(
+                CustomTabColorSchemeParams.Builder()
+                    .setToolbarColor(primaryVariant.toArgb())
+                    .build()
+            )
             .setShowTitle(true)
             .build()
+            .apply {
+                intent.setPackage(packageName)
+                launchUrl(context, uri)
+            }
+    }
 
-        CustomTabsHelper.addKeepAliveExtra(context, intent.intent)
-        CustomTabsHelper.openCustomTab(context, intent, uri, WebViewFallback())
+    private companion object {
+        const val packageName = "com.android.chrome"
     }
 }
