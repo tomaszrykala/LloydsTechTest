cbd85de62667f941613ce3553ba76833dc4c3bd2
Tomasz Rykała <tomasz.a.rykala@gmail.com>
2022-08-30 08:36:41 +0100

    Use StateFlow and hide keyboard on search (#2)

--- app/src/main/java/com/tomaszrykala/githubbrowser/compose/ui/MainActivityComposer.kt
+++ app/src/main/java/com/tomaszrykala/githubbrowser/compose/ui/MainActivityComposer.kt
@@ -22,6 +22,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.key.onKeyEvent
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.style.TextAlign
@@ -60,6 +61,7 @@ class MainActivityComposer {
                     .fillMaxWidth()
             )
 
+            val focusManager = LocalFocusManager.current
             var searchQuery by rememberSaveable(stateSaver = TextFieldValue.Saver) {
                 mutableStateOf(TextFieldValue(""))
             }
@@ -77,6 +79,7 @@ class MainActivityComposer {
                     .onKeyEvent {
                         if (it.nativeKeyEvent.keyCode == KeyEvent.KEYCODE_ENTER) {
                             controller.searchRepos(searchQuery.text)
+                            focusManager.clearFocus()
                         }
                         false
                     },
@@ -84,7 +87,10 @@ class MainActivityComposer {
                 placeholder = { Text("Search by name") },
                 keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
                 keyboardActions = KeyboardActions(
-                    onDone = { controller.searchRepos(searchQuery.text) }
+                    onDone = {
+                        controller.searchRepos(searchQuery.text)
+                        focusManager.clearFocus()
+                    }
                 ),
             )
         }
